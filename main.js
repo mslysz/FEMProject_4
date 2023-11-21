ff2524a234027fa7a4c3f72c77e5ee080869003f
mslysz <macslysz@gmail.com>
2023-11-21 03:39:22 +0000

    fixed verification problem

--- main.js
+++ main.js
@@ -22,146 +22,168 @@ const cvcElement = document.querySelector('.js-cvc-input');
 const clearInput = document.querySelectorAll('input');
 const inputElements = [...clearInput];
 
-
 //Submited message
 const informationWindow = document.querySelector('.js-form-submited');
 const containerElement = document.querySelector('.js-container-bottom');
 
 function displayTextName() {
-  if (cardHolder.value.length === 0 || cardHolder.value.length === "") {
-    cardHolderSpan.innerHTML = "JANE APPLESEED";
-    nameError.innerHTML = "Name must be filled out";
-    cardHolderSpan.classList.add('active');
-  }
   cardHolderSpan.innerHTML = cardHolder.value.toUpperCase();
-  cardHolderSpan.classList.remove('active');
+  if (cardHolder.value !== '') {
+    nameError.classList.remove('active');
+  } else {
+    cardHolderSpan.innerHTML = 'JANE APPLESEED';
+  }
 }
 
 function displayTextNumber() {
-  let text = cardNumber.value.replace(/\s/g, "");
-  let spacedText = text.replace(/(.{4})/g, "$1 ");
-  if (cardNumber.value === "") {
-    cardNumberSpan.innerHTML = "0000 0000 0000 0000";
-  } else if (isNaN(cardNumber.value)) {
-    cardError.innerHTML = "Wrong format, numbers only"
-    cardError.classList.add('active')
-  }
-  else if (text.length !== 16) {
-    cardError.innerHTML = "The card number must contain 16 characters"
-    cardError.classList.add('active');
-  }
-  else {
-    cardNumberSpan.innerHTML = spacedText
-    cardError.classList.remove('active');
+  let text = cardNumber.value.replace(/\s/g, '');
+  let spacedText = text.replace(/(.{4})/g, '$1 ');
+  if (cardNumber.value === '') {
+    cardNumberSpan.innerHTML = '0000 0000 0000 0000';
+  } else {
+    cardNumberSpan.innerHTML = spacedText;
   }
 }
+
 function displayMonthNumber() {
-  if (monthElement.value === "") {
-    cardMonthSpan.innerHTML = "00";
-    monthError.classList.remove('active');
-  } else if (isNaN(monthElement.value)) {
-    monthError.innerHTML = "Use numbers only";
-    monthError.classList.add('active');
-  }
-  else {
+  if (monthElement.value === '') {
+    cardMonthSpan.innerHTML = '00';
+  } else {
     cardMonthSpan.innerHTML = monthElement.value;
-    monthError.classList.remove('active');
   }
 }
 
 function displayYearNumber() {
-  if (yearElement.value === "") {
-    cardYearSpan.innerHTML = "00";
-    yearError.classList.remove('active');
-  } else if (isNaN(yearElement.value)) {
-    yearError.innerHTML = "Use numbers only";
-    yearError.classList.add('active');
-  }
-  else {
+  if (yearElement.value === '') {
+    cardYearSpan.innerHTML = '00';
+  } else {
     cardYearSpan.innerHTML = yearElement.value;
   }
 }
 
 function displayCVCNumber() {
-  if (cvcElement.value === "") {
-    cardCVCSpan.innerHTML = "000"
-    cvcErorr.classList.remove('active');
-  } else if (isNaN(cvcElement.value)) {
-    cvcErorr.innerHTML = "Use numbers only";
-    cvcErorr.classList.add('active');
-  }
-  else {
-    cvcErorr.classList.remove('active');
+  if (cvcElement.value === '') {
+    cardCVCSpan.innerHTML = '000';
+  } else {
     cardCVCSpan.innerHTML = cvcElement.value;
-
   }
 }
 
 const validateForm = (e) => {
   e.preventDefault();
 
-  if (cardHolder.value == "") {
-    nameError.innerHTML = "Name must be filled out";
-    nameError.classList.add('active');
-    return false
-  }
-  if (cardNumber.value === "") {
-    cardError.innerHTML = "Card number must be filled out ";
-    cardError.classList.add('active');
-    return false
-  }
-  if (monthElement.value === "") {
-    monthError.innerHTML = "Can't be blank";
-    monthError.classList.add('active');
-    return false
-  }
-  if (yearElement.value === "") {
-    yearError.innerHTML = "Can't be blank";
-    yearError.classList.add('active');
-    return false
-  }
-  if (cvcElement.value === "") {
-    cvcErorr.innerHTML = "Can't be blank";
-    cvcErorr.classList.add('active');
-    return false
-  }
-  else {
-    nameError.classList.remove('active');
-    cardError.classList.remove('active');
-    yearError.classList.remove('active');
-    monthError.classList.remove('active');
-    cvcErorr.classList.remove('active');
+  let isValid = true;
 
+  if (cardHolder.value == '') {
+    nameError.innerHTML = 'Name must be filled out';
+    nameError.classList.add('active');
+    cardHolder.classList.add('error');
+    isValid = false;
+  } else {
+    nameError.classList.remove('active');
+    cardHolder.classList.remove('error');
+  }
+
+  if (cardNumber.value === '') {
+    cardError.innerHTML = 'Card number must be filled out ';
+    cardError.classList.add('active');
+    cardNumber.classList.add('error');
+    isValid = false;
+  } else if (!/^\d+$/.test(cardNumber.value)) {
+    cardError.innerHTML = 'Wrong format, numbers only';
+    cardError.classList.add('active');
+    cardNumber.classList.add('error');
+    isValid = false;
+  } else if (cardNumber.value.length !== 16) {
+    cardError.innerHTML = 'The card number must contain 16 characters';
+    cardError.classList.add('active');
+    cardNumber.classList.add('error');
+    isValid = false;
+  } else {
+    cardError.classList.remove('active');
+    cardNumber.classList.remove('error');
+  }
+
+  if (monthElement.value === '') {
+    monthError.innerHTML = 'Month must be filled out';
+    monthError.classList.add('active');
+    monthElement.classList.add('error');
+    isValid = false;
+  } else if (!/^\d+$/.test(monthElement.value)) {
+    monthError.innerHTML = 'Use numbers only';
+    monthError.classList.add('active');
+    monthElement.classList.add('error');
+    isValid = false;
+  } else if (monthElement.value > 12) {
+    monthError.innerHTML = 'Month cannot be greater than 12';
+    monthError.classList.add('active');
+    monthElement.classList.add('error');
+    isValid = false;
+  } else {
+    monthError.classList.remove('active');
+    monthElement.classList.remove('error');
+  }
+
+  if (yearElement.value === '') {
+    yearError.innerHTML = 'Year must be filled out';
+    yearError.classList.add('active');
+    yearElement.classList.add('error');
+    isValid = false;
+  } else if (!/^\d+$/.test(yearElement.value)) {
+    yearError.innerHTML = 'Use numbers only';
+    yearError.classList.add('active');
+    yearElement.classList.add('error');
+    isValid = false;
+  } else {
+    yearError.classList.remove('active');
+    yearElement.classList.remove('active');
+  }
+
+  if (cvcElement.value === '') {
+    cvcErorr.innerHTML = 'CVC must be filled out';
+    cvcErorr.classList.add('active');
+    cvcElement.classList.add('error');
+    isValid = false;
+  } else if (!/^\d+$/.test(cvcElement.value)) {
+    cvcErorr.innerHTML = 'Use numbers only';
+    cvcErorr.classList.add('active');
+    cvcElement.classList.add('error');
+    isValid = false;
+  } else {
+    cvcErorr.classList.remove('active');
+    cvcElement.classList.remove('error');
+  }
+  if (!isValid) {
+    e.preventDefault();
+  } else {
     containerElement.classList.add('offWindow');
     informationWindow.classList.add('onWindow');
   }
-  return true
-}
+  return true;
+};
 
 const setDefaultSpanValues = () => {
-  cardHolderSpan.innerHTML = "JANE APPLESEED"
-  cardNumberSpan.innerHTML = "0000 0000 0000 0000";
-  cardMonthSpan.innerHTML = "00";
-  cardYearSpan.innerHTML = "00";
-  cardCVCSpan.innerHTML = "000"
-}
+  cardHolderSpan.innerHTML = 'JANE APPLESEED';
+  cardNumberSpan.innerHTML = '0000 0000 0000 0000';
+  cardMonthSpan.innerHTML = '00';
+  cardYearSpan.innerHTML = '00';
+  cardCVCSpan.innerHTML = '000';
+};
 
 const clearInputFields = (inputElements) => {
   const inputsExceptLast = inputElements.slice(0, inputElements.length - 1);
   inputsExceptLast.forEach((input) => {
-    input.value = "";
-  })
-}
+    input.value = '';
+  });
+};
 const resetForm = () => {
-  clearInputFields(inputElements)
-  setDefaultSpanValues()
-  console.log("Form has been reset")
+  clearInputFields(inputElements);
+  setDefaultSpanValues();
+  console.log('Form has been reset');
 
   containerElement.classList.remove('offWindow');
   informationWindow.classList.remove('onWindow');
-}
-
-
+};
 
 cardHolder.addEventListener('input', displayTextName);
 cardNumber.addEventListener('input', displayTextNumber);
